b08ca2e3a175cdb10d457e4ee8df41bf4d17a0ef
Amit Bashan <amitba@netanya.ac.il>
2024-10-29 17:26:10 +0200

    Add latest training script

--- model/script.py
+++ model/script.py
@@ -3,12 +3,15 @@ import json
 import pandas as pd
 from onnxruntime.quantization import quantize_dynamic
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
 from transformers import BertTokenizerFast, BertForSequenceClassification
 import torch.utils
 import torch.utils.data
 import tqdm
 
-EPOCHS = 2
+EPOCHS = 5
+BATCH_SIZE=16
 DEVICE = torch.device("mps")
 MODEL_NAME = "avichr/heBERT"
 DATASET_PATH = "./dataset.json"
@@ -42,12 +45,12 @@ class SmsDataset(torch.utils.data.Dataset):
 df = load_dataset()
 x = df["message"]
 y = df["spam"].astype(int)
-train_texts, test_texts, train_labels, test_labels = train_test_split(list(x), list(y), test_size=0.5)
+train_texts, test_texts, train_labels, test_labels = train_test_split(list(x), list(y), test_size=0.1)
 train_dataset = SmsDataset(train_texts, train_labels)
 test_dataset = SmsDataset(test_texts, test_labels)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=False)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 model = BertForSequenceClassification.from_pretrained(MODEL_NAME).to(DEVICE)
 optim = torch.optim.AdamW(model.parameters(), lr=5e-5)
@@ -86,23 +89,36 @@ def measure_accuracy(model):
 
 def train(model, epochs):
     model.train()
+    losses = []
 
     for epoch in tqdm.tqdm(range(epochs)):
-        for batch in train_loader:
+        for batch in tqdm.tqdm(train_loader):
             optim.zero_grad()
             input_ids = batch['input_ids'].to(DEVICE)
             attention_mask = batch['attention_mask'].to(DEVICE)
-            print(attention_mask)
             labels = batch['labels'].to(DEVICE)
             outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
             loss = outputs[0]
+            losses.append(loss.item())
             loss.backward()
             optim.step()
 
+    return losses
+
 print("Training...")
-train(model, EPOCHS)
+losses = train(model, EPOCHS)
+
+plt.plot(losses)
+plt.ylabel("Loss")
+plt.xlabel("Batch number")
+plt.show()
+
 print("Finished training.")
 print(f"Model accuracy: {measure_accuracy(model)}")
+
+y_actual, y_pred, _ = validation(model, test_loader)
+cm = confusion_matrix(y_actual, y_pred)
+print(cm)
 
 x = torch.randn(1, MAX_LENGTH, dtype=torch.long, device=DEVICE)
 attention_mask = torch.randn(1, MAX_LENGTH, dtype=torch.long, device=DEVICE)
